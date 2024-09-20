05a3d7e6dd702b9297516dfaf3b2d6859f7b0d5b
Daniel <danielcylee@outlook.com>
2024-09-20 13:59:46 +0800

    change log to append mode

--- stock_wo_pandas.py
+++ stock_wo_pandas.py
@@ -7,7 +7,7 @@ from line_notify import notify
 import traceback
 import logging
 
-logging.basicConfig(level=logging.INFO, filename='log.txt', filemode='w',
+logging.basicConfig(level=logging.INFO, filename='log.txt', filemode='a',
 	format='[%(asctime)s %(levelname)-8s] %(message)s',
 	datefmt='%Y%m%d %H:%M:%S',
 	)
